35337b2056f25e6960cb07be0f2d1e960f5ba349
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2021-10-22 15:31:03 +0900

    Create 10989.cpp

---
+++ 10989.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	// 
+	int* arr;
+
+	return 0;
+}
