699a5b71ac8bc4a4affcb0cf1feca19a995c41a2
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2023-04-10 00:09:22 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <list>
+#include <cstdlib>
+#include <queue>
+#include <deque>
+#include<algorithm>
+#include <map>
+using namespace std;
+long long N,S;
+long long arr[100001]; 
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+char W[10001];
+cin.getline(W, 10001, EOF);
+cout << W;
+
+	return 0;
+}
