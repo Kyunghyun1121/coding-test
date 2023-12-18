a23a5ec25f14a9f4bf01988781e389972e9696b1
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2023-12-18 21:43:46 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        long A,B,C;
+        Scanner scanner = new Scanner(System.in);
+
+        A = scanner.nextLong();
+        B = scanner.nextLong();
+        C = scanner.nextLong();
+        System.out.println(A+B+C);
+
+
+
+    }
+}
