3b7b523bfe58780a622e2e9a718c647c1e2b21b9
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2024-01-05 12:48:43 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 212 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        long cas = scanner.nextInt();
+
+        long out = cas * cas;
+        System.out.println(out);
+        System.out.println(2);
+    }
+
+}
