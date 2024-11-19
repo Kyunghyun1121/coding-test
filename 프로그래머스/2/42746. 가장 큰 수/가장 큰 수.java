b2d0b6156c13b79f358c6f0e0b4c77c626fbc546
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2024-11-19 22:34:38 +0900

    [level 2] Title: 가장 큰 수, Time: 168.58 ms, Memory: 139 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        String[]arr = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++){
+            arr[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(arr, (a,b) -> (b+a).compareTo(a+b));
+        for(String s : arr){
+            sb.append(s);
+        }
+        
+        
+
+        if (sb.toString().charAt(0) == '0') {
+            return "0";
+        }
+        
+        return sb.toString();
+    }
+}
