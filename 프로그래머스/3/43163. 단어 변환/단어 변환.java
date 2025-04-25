ef13ffd5ef3cbbee3d3333ce258ab6bc038c9d21
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2025-04-25 15:49:18 +0900

    [level 3] Title: 단어 변환, Time: 1.08 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,37 +1,52 @@
+import java.util.*;
+
 class Solution {
-    String[]words;  String begin; String target;
-    boolean[]rem; int len; int out = 10000000;
-    
-    public void dfs(String now, int next, int count){
-        //System.out.println(now + "  " + count);
-        if(now.equals(target)){
-            out = Math.min(out, count); 
-            return;
+    class word{
+        String word;
+        int count = 0;
+        public word(String a, int b){
+            word = a;
+            count = b;
         }
-        
-        int sum = 0;
-        for(int i = 0; i < now.length(); i++)if(now.charAt(i)!=words[next].charAt(i))sum++;
-        if(sum > 1)return;
-        
-        rem[next] = true;
-        for(int i = 0; i < len; i++){
-            if(!rem[i]){
-                dfs(words[next], i, count+1);
-            }
-        }
-        rem[next] = false;
     }
     
-    public int solution(String b, String t, String[] w) {
-        int answer = 0; words = w; len = w.length;
-        begin = b; target = t;
+    boolean compare(String a, String b){
+        char[] A = a.toCharArray();
+        char[] B = b.toCharArray();
         
-        rem = new boolean[w.length+10];
-        for(int i = 0; i < len; i++){
-            dfs(begin, i, 0);
+        int c = 0;
+        for(int i = 0; i < A.length; i++){
+            if(A[i] != B[i])
+                c++;
+            if(c > 1) return false;
         }
         
-        if(out == 10000000)out = 0;
-        return out;
+        //System.out.println(a + " " + b);
+        return true;
+    }
+    
+    public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        
+        ArrayDeque<word>q = new ArrayDeque<>();
+        q.add(new word(begin, 0));
+       
+        HashSet<String>visit = new HashSet<>();
+        
+        while(!q.isEmpty()){
+            word w = q.pop();
+            if(w.word.equals(target)) return w.count;
+            
+            if(!visit.contains(w.word)){
+                for(String s : words){
+                    if(compare(s, w.word))
+                        q.add(new word(s, w.count+1));
+                }
+            }
+            
+            visit.add(w.word);
+        }
+        
+        return answer;
     }
 }
