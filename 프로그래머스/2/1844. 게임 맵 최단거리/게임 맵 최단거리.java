9aed1bdfb3011ffa57b029a7824a26d5e937f53c
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2025-05-29 14:29:52 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.64 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,54 +1,47 @@
 import java.util.*;
 
 class Solution {
-    int[]dx = {0,0,-1,1};
-    int[] dy = {-1,1,0,0};
-    
+
     public class move{
         int x;
         int y;
-        int len;
+        int count;
         public move(int a, int b, int c){
-            y = a;
             x = b;
-            len = c;
+            y = a;
+            count = c;
         }
-
-    
     }
+    int[]dx = {0,0,-1,1};
+    int[]dy = {-1,1,0,0};
     
     public int solution(int[][] maps) {
-        int answer = 100000;
-        int xLen = maps[0].length;
-        int yLen = maps.length;
-        boolean[][]rem = new boolean[yLen+1][xLen+1];
-        
         ArrayDeque<move>q = new ArrayDeque<>();
         q.add(new move(0,0,1));
+        boolean[][]rem = new boolean[maps.length][maps[0].length];
         rem[0][0] = true;
         
         while(!q.isEmpty()){
             move temp = q.remove();
-            
-            if(temp.x == xLen -1 && temp.y == yLen -1){
-                answer = Math.min(answer, temp.len);
-                continue;
+            if(temp.x == maps[0].length -1 && 
+               temp.y == maps.length - 1){
+                return temp.count;
             }
             
-            for(int i =0; i < 4; i++){
+            for(int i = 0; i < 4; i++){
                 int tx = temp.x + dx[i];
                 int ty = temp.y + dy[i];
-                if(tx >= 0 && tx < xLen &&  ty >= 0 && 
-                  ty < yLen && !rem[ty][tx] 
-                   && maps[ty][tx] == 1){
+                if(tx >= 0 && tx < maps[0].length &&
+                  ty >= 0 && ty < maps.length && 
+                   maps[ty][tx] == 1 && !rem[ty][tx]){
+                    q.add(new move(ty,tx,temp.count + 1));
                     rem[ty][tx] = true;
-                    q.add(new move(ty,tx,temp.len+1));
                 }
             }
+            
         }
         
+        return -1;
         
-        if(answer == 100000)answer = -1;
-        return answer;
     }
 }
