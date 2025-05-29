a64b16da5e9c3530c9abdc3569ae4f41cc5bc9c7
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2025-05-29 16:05:31 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 16.58 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,86 +1,66 @@
 import java.util.*;
 
 class Solution {
-    boolean[] visited = new boolean[101];
-    ArrayList<ArrayList<Integer>>connect;
-    
-    public int dfs(int n){
-        int sum = 1;
-        
-        visited[n] = true;
-        //System.out.println( n + " " + connect.get(n));
-        for(int i : connect.get(n)) {
-            if(!visited[i]){
-                //System.out.println(n + " " + dfs(i));
-                sum += dfs(i);
-            }
-        }
-        //System.out.println(n + " " + sum);
-        return sum;
-    }
+
     
     public int solution(int n, int[][] wires) {
         int answer = 100000;
         
-        connect = new ArrayList<>();
-        for(int j = 0; j <= n + 1; j++){
-            connect.add(new ArrayList<Integer>());
-            //connect.get(j).add(101);
+        ArrayList<ArrayList<Integer>>wire = new ArrayList<>();
+        for(int i = 0; i <= n; i++){
+            wire.add(new ArrayList<Integer>());
+            wire.get(i).add(101);
         }
             
-        
-        //System.out.println(connect.size());
-        for(int j = 0; j < wires.length; j++){
-            //System.out.println(wires[j][0] + " " + wires[j][1]);
-            connect.get(wires[j][0]).add(wires[j][1]);
-            connect.get(wires[j][1]).add(wires[j][0]);
+        for(int i = 0; i < wires.length; i++){
+            int one = wires[i][0] - 1;
+            int two = wires[i][1] - 1;
+            wire.get(one).add(two);
+            wire.get(two).add(one);
         }
-        
-        //print();
-            
+        //for(int i = 0; i < n; i++) System.out.println(i + " " + wire.get(i));
         
         for(int i = 0; i < wires.length; i++){
-            int cutA = wires[i][0];
-            int cutB = wires[i][1];
-            //System.out.println(connect.get(cutB).remove(cutA));
-            connect.get(cutA).remove((Integer)cutB);
-            connect.get(cutB).remove((Integer)cutA);
+            Integer one = wires[i][0] - 1;
+            Integer two = wires[i][1] - 1;
             
-            visited = new boolean[101];
-            int one = -1;
-            int two = -1;
-            //print();
+            wire.get(one).remove(two);
+            wire.get(two).remove(one);
             
-            for(int j = 1; j <= n; j++){
-                if(!visited[j]){
-                    if(one == -1) one = dfs(j);
-                    else{
-                       two = dfs(j); 
-                        break;
-                    } 
+            boolean[]rem = new boolean[n];
+            
+
+                
+                int count = 0;
+                ArrayDeque<Integer>q = new ArrayDeque<>();
+                q.add(0);
+            rem[0] = true;
+                while(!q.isEmpty()){
+                    int now = q.remove();
+                    count++;
+                    //System.out.println(now + " " + count);
+                    for(int k : wire.get(now)){
+                        if(k != 101 && !rem[k]){
+                            q.add(k);
+                            rem[k] = true;
+                        }
+                    }
                 }
-            }
+                
+                //System.out.println(count);
+            //System.out.println();
+                // if(compare == -1)compare = count;
+                // else compare = Math.abs(compare - count);
+                
             
-            // System.out.println();
-            // System.out.println(one + " " + two);
-            // System.out.println();
             
-            answer = Math.min(answer, Math.abs(one - two));
-            connect.get(cutA).add(cutB);
-            connect.get(cutB).add(cutA);
+            answer = Math.min(answer,  Math.abs(n - count - count));
+            wire.get(one).add(two);
+            wire.get(two).add(one);
         }
         
         return answer;
     }
-    public void print(){
-                int l = 0;
-        for(ArrayList<Integer> i : connect){
-            System.out.print(l + ": ");
-            for(int j : i){
-                System.out.print(j + " ");
-            }
-            System.out.println();
-            l++;
-        }
-    }
+
+    
 }
