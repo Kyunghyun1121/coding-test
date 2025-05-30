99433d7d5c50b2b0f71ef63fa22dbfe8206a0eeb
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2025-05-30 11:39:27 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.46 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,66 +1,50 @@
 import java.util.*;
 
 class Solution {
-
+    
+    int all = 0;
+    ArrayList<ArrayList<Integer>>wire = new ArrayList<>();
+    int out = 100000;
     
     public int solution(int n, int[][] wires) {
-        int answer = 100000;
+        int answer = -1;
+        all = n;
+        int root = -1;
         
-        ArrayList<ArrayList<Integer>>wire = new ArrayList<>();
         for(int i = 0; i <= n; i++){
-            wire.add(new ArrayList<Integer>());
-            wire.get(i).add(101);
+            wire.add(new ArrayList<>());
         }
-            
-        for(int i = 0; i < wires.length; i++){
-            int one = wires[i][0] - 1;
-            int two = wires[i][1] - 1;
-            wire.get(one).add(two);
-            wire.get(two).add(one);
-        }
-        //for(int i = 0; i < n; i++) System.out.println(i + " " + wire.get(i));
         
         for(int i = 0; i < wires.length; i++){
-            Integer one = wires[i][0] - 1;
-            Integer two = wires[i][1] - 1;
-            
-            wire.get(one).remove(two);
-            wire.get(two).remove(one);
-            
-            boolean[]rem = new boolean[n];
-            
-
-                
-                int count = 0;
-                ArrayDeque<Integer>q = new ArrayDeque<>();
-                q.add(0);
-            rem[0] = true;
-                while(!q.isEmpty()){
-                    int now = q.remove();
-                    count++;
-                    //System.out.println(now + " " + count);
-                    for(int k : wire.get(now)){
-                        if(k != 101 && !rem[k]){
-                            q.add(k);
-                            rem[k] = true;
-                        }
-                    }
-                }
-                
-                //System.out.println(count);
-            //System.out.println();
-                // if(compare == -1)compare = count;
-                // else compare = Math.abs(compare - count);
-                
-            
-            
-            answer = Math.min(answer,  Math.abs(n - count - count));
+            int one = wires[i][0];
+            int two = wires[i][1];
             wire.get(one).add(two);
             wire.get(two).add(one);
         }
+        
+        root = 1;
+        for(int i = 0; i <= n; i++){
+            if(root == i)continue;
+            if(wire.get(root).size() < wire.get(i).size())root = i;
+        }
+        int t = dfs(root, new boolean[n+1]);
+        
+        return out;
+    }
+    
+    
+    public int dfs(int num, boolean[]rem){
+        int answer = 1;
+        rem[num] = true;
+        for(int t : wire.get(num)){
+            if(!rem[t]){
+                answer += dfs(t, rem);
+            }
+        }
+        rem[num] = false;
+        out = Math.min(out, Math.abs(all - answer - answer));
         
         return answer;
+        
     }
-
-    
 }
