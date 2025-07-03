0aeac872af5d5e034e60c29401c0a95ebad11785
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2025-07-03 16:35:53 +0900

    [level 2] Title: 소수 찾기, Time: 1.33 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,57 +1,60 @@
 import java.util.*;
 
 class Solution {
-    char[] arr;
-    int answer = 0;
-    boolean[] c = new boolean[1000000000];
+    char[]arr;
+    boolean[]rem;
     
-    public void turn(int k, int count, int[]a, boolean[] rem){
-        if(count == k){
-            String temp = "";
-            for(int i : a)temp += arr[i];
-            
-            int n = Integer.valueOf(temp);
-            
-            if(!c[n] && check(n)){
-                //System.out.println(temp + " " + n);
+    int answer = 0;
+    boolean[]ansRem = new boolean[10000001];
+    
+    public void dfs(int num, int n){
+        if(n > arr.length) return;
+        
+        if(!ansRem[num]){
+            if(check(num)){
+                ansRem[num] = true;
                 answer++;
-                c[n] = true;
             }
+        }
+        
+        //System.out.println(num);
+
+        for(int i = 0; i < arr.length; i++){
+            if(rem[i])
+                continue;
             
-            return;
-        }else{
-            for(int i = 0; i < arr.length; i++){
-                if(rem[i])continue;
-                
-                a[count] = i;
-                rem[i] = true;
-                turn(k, count + 1, a, rem);
-                rem[i] = false;
-            }
+            int now = arr[i] - '0';
+            rem[i] = true;
+            
+            dfs(num*10 + now, n+1);
+
+            rem[i] = false;
+            
         }
     }
     
-    
     public int solution(String numbers) {
-        
-        
         arr = numbers.toCharArray();
-        for(int i = 1; i <= numbers.length(); i++){
-            turn(i, 0, new int[i], new boolean[10]);
+        rem = new boolean[numbers.length()];
+        
+        for(int i = 0; i < numbers.length(); i++){
+            rem[i] = true;
+            dfs(numbers.charAt(i) - '0', 1);
+            rem[i] = false;
         }
+        
+        
         
         return answer;
     }
-    
     public boolean check(int num){
-        if(num == 0 || num == 1)return false;
-        
-        int t = (int)Math.sqrt(num) + 1;
-        
-        for(int i = 2; i <= t; i++){
-            if(num != i && num % i == 0)return false;
+        if(num == 1 || num == 0)return false;
+        for(int i = 2; i * i <= num; i++){
+            if(num % i == 0)
+                return false;
         }
         
         return true;
     }
+    
 }
