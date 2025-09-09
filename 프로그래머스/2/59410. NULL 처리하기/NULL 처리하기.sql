d12dbfc283181a3bf924e8c662b46854c1657502
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2025-09-09 20:53:27 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,7 +1,6 @@
 -- 코드를 입력하세요
-select ANIMAL_TYPE, 
-    case when NAME is null then "No name"
-        else NAME end as NAME, 
-    SEX_UPON_INTAKE
-from ANIMAL_INS 
-order by ANIMAL_ID
+SELECT ANIMAL_TYPE, 
+    case 
+        when NAME is null then "No name"
+        else NAME end as NAME, SEX_UPON_INTAKE
+from ANIMAL_INS
