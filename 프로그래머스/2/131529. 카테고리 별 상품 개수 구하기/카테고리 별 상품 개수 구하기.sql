c71286c37a9c14ea6bc4145c431c3edc51513823
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2025-09-10 20:08:35 +0900

    [level 2] Title: 카테고리 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131529. 카테고리 별 상품 개수 구하기/카테고리 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131529. 카테고리 별 상품 개수 구하기/카테고리 별 상품 개수 구하기.sql
@@ -1,5 +1,4 @@
 -- 코드를 입력하세요
-select left(PRODUCT_CODE, 2) as CATEGORY, count(PRODUCT_ID) as PRODUCTS
+SELECT substring(PRODUCT_CODE, 1, 2) as "CATEGORY", count(PRODUCT_ID) as "PRODUCTS"
 from PRODUCT
-group by left(PRODUCT_CODE, 2)
-order by CATEGORY asc
+group by substring(PRODUCT_CODE, 1, 2)
