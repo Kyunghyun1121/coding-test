b251a42218284536201a62714b32b3fcf4a22cf9
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2025-09-10 21:46:46 +0900

    [level 3] Title: 단어 변환, Time: 1.43 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java#) 
 
 ### 성능 요약
 
-메모리: 73 MB, 시간: 15.32 ms
+메모리: 84.9 MB, 시간: 1.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 29일 15:12:56
+2025년 09월 10일 21:46:44
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,48 +1,78 @@
 import java.util.*;
 
 class Solution {
-
-    public boolean avail(String a, String b){
-        int count = 0;
-        for(int i = 0; i < a.length(); i++){
-            if(a.charAt(i) != b.charAt(i)){
-                count++;
-                if(count >= 2)return false;
-            }
-        }
-        return true;
-    }
-    
-    public class word{
+    public class Word{
+        String word;
         int count;
-        HashSet<String> rem;
-        String now;
-        public word(HashSet<String> a,String c, int b){
-            rem = a;
-            now = c;
-            rem.add(c);
-            count = b;
+        public Word(String a, int b){
+            word = a; count = b;
         }
     }
     
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
 
-        ArrayDeque<word>q = new ArrayDeque<>();
-        q.add(new word(new HashSet<String>(), begin, 0));
-        while(!q.isEmpty()){
-            word temp = q.remove();
-            System.out.println(temp.now + " " + temp.count);
-            
-            if(temp.now.equals(target)) return temp.count;
-            for(String t : words){
-                if(!temp.rem.contains(t) && avail(temp.now, t)){
-                    q.add(new word(temp.rem, t, temp.count + 1));
+        HashMap<String, Integer>dict = new HashMap<>();
+        for(int i = 0; i < words.length; i++) dict.put(words[i], i);
+        if(!dict.containsKey(target)) return 0;
+        
+        ArrayList<ArrayList<String>>list = new ArrayList<>();
+        for(int i = 0; i < words.length; i++) list.add(new ArrayList<>());
+        
+        for(int i = 0; i < words.length; i++){
+            for(int j = i + 1; j < words.length; j++){
+                if(changeable(words[i], words[j])){
+                    int a = dict.get(words[i]);
+                    int b = dict.get(words[j]);
+                    list.get(a).add(words[j]);
+                    list.get(b).add(words[i]);
                 }
             }
         }
         
+        boolean[]visit = new boolean[words.length + 1];
+        ArrayDeque<Word> q = new ArrayDeque<>();
+        for(String temp : words){
+            if(temp.equals(begin)){
+                q.add(new Word(temp, 0));
+            }
+            else if(changeable(begin, temp)){
+                q.add(new Word(temp, 1));
+            }
+        }
+
+        
+        while(!q.isEmpty()){
+            Word t = q.remove();
+            //System.out.println(dict.get(t.word));
+            if(t.word.equals(target)) return t.count;
+            if(visit[dict.get(t.word)]) continue;
+            
+            
+            for(String temp : list.get(dict.get(t.word))){
+                if(temp.equals(target)) return t.count + 1;
+                if(!visit[dict.get(temp)]){
+                    visit[dict.get(t.word)] = true;
+                    q.add(new Word(temp, t.count + 1));   
+                }
+            }
+        }
+        
+        //System.out.println(list);
         
         return answer;
+    }
+    
+    public boolean changeable(String a, String b){
+        char[]at = a.toCharArray();
+        char[]bt = b.toCharArray();
+        int count = 0;
+        for(int i = 0; i < at.length; i++){
+            if(at[i] != bt[i]){
+                count++;
+                if(count >= 2) return false;
+            } 
+        }
+        return true;
     }
 }
