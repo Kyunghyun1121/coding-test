389a0eb6f32c7e0df58d81045c08246ace694c6b
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2025-09-17 21:27:10 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.85 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,45 +2,37 @@ import java.util.*;
 
 class Solution {
 
-    public class move{
-        int x;
-        int y;
-        int count;
-        public move(int a, int b, int c){
-            x = b;
-            y = a;
-            count = c;
-        }
-    }
     int[]dx = {0,0,-1,1};
     int[]dy = {-1,1,0,0};
+    public class move{
+        int x; int y;
+        int len;
+        public move(int a, int b,int c){
+            y = a;
+            x = b;
+            len = c;
+        }
+    }
     
     public int solution(int[][] maps) {
-        ArrayDeque<move>q = new ArrayDeque<>();
-        q.add(new move(0,0,1));
-        boolean[][]rem = new boolean[maps.length][maps[0].length];
-        rem[0][0] = true;
         
+        ArrayDeque<move>q = new ArrayDeque<>();
+        boolean[][]visited = new boolean[maps.length][maps[0].length];
+        q.add(new move(0,0,0));
+        visited[0][0] = true;
         while(!q.isEmpty()){
-            move temp = q.remove();
-            if(temp.x == maps[0].length -1 && 
-               temp.y == maps.length - 1){
-                return temp.count;
-            }
-            
+            move t = q.remove();
+            if(t.x == maps[0].length - 1&& t.y == maps.length - 1) return t.len + 1;
             for(int i = 0; i < 4; i++){
-                int tx = temp.x + dx[i];
-                int ty = temp.y + dy[i];
-                if(tx >= 0 && tx < maps[0].length &&
-                  ty >= 0 && ty < maps.length && 
-                   maps[ty][tx] == 1 && !rem[ty][tx]){
-                    q.add(new move(ty,tx,temp.count + 1));
-                    rem[ty][tx] = true;
+                int ty = t.y + dy[i];
+                int tx = t.x + dx[i];
+                if(tx >= 0 && tx < maps[0].length && ty >= 0 && ty < maps.length
+                  && !visited[ty][tx] && maps[ty][tx] != 0){
+                    visited[ty][tx] = true;
+                    q.add(new move(ty, tx, t.len + 1));
                 }
             }
-            
         }
-        
         return -1;
         
     }
