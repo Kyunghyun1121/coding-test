76fd980870043d116b6e040721f72c0d24ac616a
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2025-09-19 21:34:27 +0900

    [level 3] Title: 단어 변환, Time: 0.47 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java) 
 
 ### 성능 요약
 
-메모리: 84.9 MB, 시간: 1.43 ms
+메모리: 70.3 MB, 시간: 0.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 10일 21:46:44
+2025년 09월 19일 21:34:28
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,78 +1,50 @@
 import java.util.*;
 
 class Solution {
-    public class Word{
-        String word;
-        int count;
-        public Word(String a, int b){
-            word = a; count = b;
+    String[] word;
+    int ansC = 100000001;
+    HashSet<String>check = new HashSet<>();
+    public void dfs(String now, String target, int count){
+        if(now.equals(target)){
+            //System.out.println(ans);
+            if(ansC > count){
+                ansC = count;
+            }
+            return;
+        }
+        
+        for(String temp : word){
+            if(changeAble(temp, now) && !check.contains(temp)){
+                if(!target.equals(temp))check.add(temp);
+                dfs(temp, target, count + 1);
+            }
         }
     }
+    
     
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-
-        HashMap<String, Integer>dict = new HashMap<>();
-        for(int i = 0; i < words.length; i++) dict.put(words[i], i);
-        if(!dict.containsKey(target)) return 0;
+        word = words;
+        boolean c = false;
+        for(String s : words)   if(target.equals(s)) c = true;
+        if(!c) return 0;
         
-        ArrayList<ArrayList<String>>list = new ArrayList<>();
-        for(int i = 0; i < words.length; i++) list.add(new ArrayList<>());
-        
-        for(int i = 0; i < words.length; i++){
-            for(int j = i + 1; j < words.length; j++){
-                if(changeable(words[i], words[j])){
-                    int a = dict.get(words[i]);
-                    int b = dict.get(words[j]);
-                    list.get(a).add(words[j]);
-                    list.get(b).add(words[i]);
-                }
-            }
-        }
-        
-        boolean[]visit = new boolean[words.length + 1];
-        ArrayDeque<Word> q = new ArrayDeque<>();
-        for(String temp : words){
-            if(temp.equals(begin)){
-                q.add(new Word(temp, 0));
-            }
-            else if(changeable(begin, temp)){
-                q.add(new Word(temp, 1));
-            }
-        }
-
-        
-        while(!q.isEmpty()){
-            Word t = q.remove();
-            //System.out.println(dict.get(t.word));
-            if(t.word.equals(target)) return t.count;
-            if(visit[dict.get(t.word)]) continue;
-            
-            
-            for(String temp : list.get(dict.get(t.word))){
-                if(temp.equals(target)) return t.count + 1;
-                if(!visit[dict.get(temp)]){
-                    visit[dict.get(t.word)] = true;
-                    q.add(new Word(temp, t.count + 1));   
-                }
-            }
-        }
-        
-        //System.out.println(list);
-        
-        return answer;
+        dfs(begin, target, 0);
+        if(ansC == 100000001)return 0;
+        return ansC;
     }
     
-    public boolean changeable(String a, String b){
-        char[]at = a.toCharArray();
-        char[]bt = b.toCharArray();
+    public boolean changeAble(String a, String b){
         int count = 0;
+        char[] at = a.toCharArray();
+        char[] bt = b.toCharArray();
         for(int i = 0; i < at.length; i++){
-            if(at[i] != bt[i]){
-                count++;
-                if(count >= 2) return false;
-            } 
+            if(at[i] != bt[i]) count++;
+            if(count > 1) return false;
         }
         return true;
+        
     }
+    
+
 }
