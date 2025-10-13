ae45ff68a9f29e361b6ec8769c117e6ee5c6aba0
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2025-10-13 15:36:36 +0900

    [level 1] Title: 과일로 만든 아이스크림 고르기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
+++ 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
@@ -1,6 +1,7 @@
 -- 코드를 입력하세요
-SELECT distinct f.FLAVOR
-from (select distinct * from FIRST_HALF where TOTAL_ORDER > 3000) as f
-    , (select distinct * from ICECREAM_INFO where INGREDIENT_TYPE = 'fruit_based')as i
-where i.FLAVOR = f.FLAVOR
-order by f.TOTAL_ORDER desc
+SELECT a.FLAVOR
+from FIRST_HALF as a join (select * from ICECREAM_INFO where INGREDIENT_TYPE like 'fruit_based')
+    as b on a.FLAVOR = b.FLAVOR
+group by  a.FLAVOR
+having sum(a.TOTAL_ORDER) >= 3000
+order by sum(a.TOTAL_ORDER) desc
