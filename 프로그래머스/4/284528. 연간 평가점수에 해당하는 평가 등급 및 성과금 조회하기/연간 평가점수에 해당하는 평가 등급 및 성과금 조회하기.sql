df8405e1a9c97d98a9d373b512605847b6eca675
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2025-10-20 19:48:53 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,14 +1,15 @@
 -- 코드를 작성해주세요
-select e.EMP_NO,	e.EMP_NAME,	g.GRADE,
- (case when g.GRADE like 'S' then e.SAL * 0.2
-      when g.GRADE like 'A' then e.SAL * 0.15
-      when g.GRADE like 'B' then e.SAL * 0.1
-     else 0 end) as BONUS
-from HR_EMPLOYEES as e, 
- (select EMP_NO,
-     (case when round(avg(SCORE),5) >= 96.0 then 'S'
-       when round(avg(SCORE),5) >= 90.0 then 'A'
-       when round(avg(SCORE),5) >= 80.0 then 'B'
-     else 'C' end) as GRADE from HR_GRADE group by EMP_NO) as g
-where e.EMP_NO = g.EMP_NO
-order by e.EMP_NO asc
+select a.EMP_NO, a.EMP_NAME ,
+    case when avg(b.SCORE) >= 96 then 'S'
+        when avg(b.SCORE) >= 90 then 'A'
+        when avg(b.SCORE) >= 80 then 'B'
+        else 'C'
+    end as GRADE,
+    case when avg(b.SCORE) >= 96 then a.SAL * 0.2
+        when avg(b.SCORE) >= 90 then a.SAL * 0.15
+        when avg(b.SCORE) >= 80 then a.SAL * 0.1
+        else 0
+    end as BONUS
+from HR_EMPLOYEES as a join HR_GRADE as b on a.EMP_NO = b.EMP_NO
+group by a.EMP_NO
+order by a.EMP_NO
