c2b63c76b9e59119325943f513a995462cd3a731
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2025-10-29 21:39:35 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,36 +3,29 @@ import java.util.*;
 class Solution {
     public ArrayDeque<Integer> solution(int[] progresses, int[] speeds) {
         int[] answer = {};
+        int days = 0;
         
-        int[]needs = new int[speeds.length];
-        for(int i = 0; i < speeds.length; i++){
-            needs[i] = (100-progresses[i]) / speeds[i];
-            if((100-progresses[i]) % speeds[i] > 0)needs[i]++;
-        }
-        //for(int i : needs)
-         //   System.out.print(i + " ");
-        
-        //System.out.println();
-        
-        ArrayDeque<Integer>time = new ArrayDeque<>();
-        int wait = needs[0];
-        int count = 1;
-        
-        for(int i = 1; i < speeds.length; i++){
-            if(needs[i] > wait){
-                time.add(count);
-                count = 1;
-                wait = needs[i];
+        ArrayDeque<Integer>q = new ArrayDeque<>();
+        int nowDay = 1;
+        for(int i = 0; i < progresses.length;){
+            int remain = 100 - progresses[i];
+            if(speeds[i] * nowDay >= remain){
+                if(i == progresses.length - 1){
+                    q.add(days+1);
+                }
+                days++;
+                i++;
             }else{
-                count++;
+                if(days != 0){
+                    q.add(days);
+                    days = 0;
+                }
+                nowDay++;
             }
         }
         
-        time.add(count);
-        
-        //System.out.println(time);
         
         
-        return time;
+        return q;
     }
 }
